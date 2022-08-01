800a80d038ecd74430b2f0501572b4cf05a95898
Aimee <aimeenyy@gmail.com>
2022-08-01 11:33:31 -0700

    initial commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,8 +7,18 @@ export default function Intro() {
                 <h1>My name is <span className="highlight">Aimee Young</span>, 
                 and I'm <br/>a <span className="highlight">web
                 developer</span>.</h1>
+                <p> 
+                I have experience in developing projects from start to finish, back-end to front-end, creating meaningful and thoughtful user experiences, and connecting with customers and teammates. 
+                </p>
             </div>
-            
+            <a href="#projects" className="scroll-down">
+                <div className="mouse">
+                    <span></span>
+                </div>
+                <div className="arrow">
+                    <span></span>
+                </div>
+            </a>
         </div>
     )
 }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,20 @@
-import "./projects.scss"
+import "./projects.scss";
+import fiberamie from "./fiberamie.png";
+import dreamer from "./dreamer.png"
+import sourcebook from "./sourcebook.png"
+import chirp from "./chirp.png"
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 export default function Projects() {
     return (
-        <div className="projects" id="projects">
-            These are some of my projects
+        <div className="projects">
+            <h1 className="scroll-down">Projects</h1>
+            <div className="project-images">
+                <a href="https://github.com/aimeeny/fiberamie" rel="noreferrer" target="_blank"><img src={fiberamie} alt="fiber amie" /></a>          
+                <a href="https://github.com/PdxCodeGuild/class_koi/tree/main/code/aimee/html_css/lab03" rel="noreferrer" target="_blank"><img src={dreamer} alt="dreamer" /> </a>               
+                <a href="https://github.com/aimeeny/sourcebook" rel="noreferrer" target="_blank"><img src={sourcebook} alt="sourcebook" /></a>                
+                <a href="https://github.com/PdxCodeGuild/class_koi/tree/main/code/aimee/django/lab03/chirp" rel="noreferrer" target="_blank"><img src={chirp} alt="chirp" /></a>            
+            </div>
         </div>
     )
 }
