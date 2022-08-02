f996ec6ac3c89883b0d4b6d3ddb584e7855458ed
Aimee <aimeenyy@gmail.com>
2022-08-02 10:18:38 -0700

    completed responsive design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,9 +5,9 @@ export default function Navbar() {
     return (
         <div className="navbar">
             <a className="menu-items" href="#intro">About</a>
-            <a className="menu-items" href="#projects">Projects</a>
-            <a className="menu-items" href="#contact">Contact</a>
+            <a className="menu-items" href="#projects">Projects</a>            
             <a className="menu-items" href="#resume">Resume</a>
+            <a className="menu-items" href="#contact">Contact</a>
             {/* <Link to="/blog" className="menu-items">Blog</Link> */}
         </div>
     )
